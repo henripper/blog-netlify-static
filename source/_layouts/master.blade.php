d4110199d091b78f876a5a84970b03a8b64ce428
henripper <henri.potter@gmail.com>
2019-12-30 13:44:04 +0200

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -37,8 +37,7 @@
 
     <footer>
         <small>
-            This project is maintained by <a href="https://raniesantos.netlify.com">Ranie Santos</a>.
-            View the <a href="https://github.com/raniesantos/artisan-static">GitHub repo</a>.
+            
         </small>
     </footer>
 
