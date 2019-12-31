d830383efe66ca114b1e084af03ef0a93a741bb1
henripper <henri.potter@gmail.com>
2019-12-31 11:28:37 +0200

    Update post.blade.php

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -17,15 +17,15 @@
             {{ $loop->last ? '' : '-' }}
         @endforeach
     </p>
-
+ {{--
     <blockquote data-phpdate="{{ $page->date }}">
         <em>WARNING: This post is over a year old. Some of the information this contains may be outdated.</em>
     </blockquote>
-
+ 
     <hr>
 
     <p>DISCLAIMER: Any 3rd-party services in these posts and in the config are only recommendations/suggestions. I am not affiliated with any of them.</p>
-
+--}}
     @yield('postContent')
 
     <hr>
